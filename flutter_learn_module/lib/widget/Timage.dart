738af6f8f41d7ac73004c9cb932e2b9047e51aff
wanghao <w_hao1987@163.com>
2023-04-15 16:29:51 +0800

    添加图片视频资源文件

---
+++ flutter_learn_module/lib/widget/Timage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+///内部判断 image来源（做了一层封装）：是来自asset还是file或memory，network
+class TImage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     if (Image.) {
+
+     }
+  }
+
+}
