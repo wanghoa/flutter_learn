f051ee70cf76df1c291a961a34f9212a0351d888
wanghao <w_hao1987@163.com>
2023-04-15 21:18:56 +0800

    创建FlutterFragmentUtil工具类创建FLutterFragment

--- app/src/main/java/com/example/flutter_learn/FlutterFragmentUtil.kt
+++ app/src/main/java/com/example/flutter_learn/FlutterFragmentUtil.kt
@@ -2,12 +2,50 @@ package com.example.flutter_learn
 
 import android.content.Context
 import io.flutter.embedding.android.FlutterFragment
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+import io.flutter.plugin.common.MethodChannel
 
 object FlutterFragmentUtil {
-    private const val  HIDE_BOTTOM ="HideBottomBar"
-    private const val  TAG ="Flutter"
+    private const val HIDE_BOTTOM = "HideBottomBar"
+    private const val TAG = "Flutter"
 
-    fun createFlutterFragment(context:Context,name: String,initRoute: String ="/") : FlutterFragment {
+    fun createFlutterFragment(
+        context: Context,
+        name: String,
+        initRoute: String = "/"
+    ): FlutterFragment {
+        val id = "${MainActivity.ENGINE_ID}"
+        var flutterEngine = FlutterEngineCache.getInstance().get(id)
+        if (null == flutterEngine) {
+            flutterEngine = FlutterEngine(context)
+            flutterEngine.navigationChannel.setInitialRoute(initRoute)
+            flutterEngine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint.createDefault())
+            FlutterEngineCache.getInstance().put(id, flutterEngine)
+            setMethodChannel(context, flutterEngine)
+        }
+//        flutterEngine.plugins.add(SharedPreferencesPlugin())
+        return FlutterFragment.withCachedEngine(id).shouldAttachEngineToActivity(true)
+            .build() as FlutterFragment
+
+    }
+
+    private fun setMethodChannel(context: Context, flutterEngine: FlutterEngine) {
+        MethodChannel(
+            flutterEngine.dartExecutor,
+            "CommonChannel"
+        ).setMethodCallHandler { call, resutl ->
+            when (call.method) {
+                HIDE_BOTTOM -> {
+                    (context as MainActivity).hideBottomButton(call.argument<Boolean>("hide") == true)
+                }
+                else -> {
+                    resutl.notImplemented()
+                }
+
+            }
+        }
 
 
     }
