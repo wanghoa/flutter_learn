9c21ac207f357e7028331239b9e5bf3cf359432b
wanghao <w_hao1987@163.com>
2023-04-15 19:20:52 +0800

    创建FlutterFragmentUtil工具类

---
+++ app/src/main/java/com/example/flutter_learn/FlutterFragmentUtil.kt
@@ -0,0 +1,14 @@
+package com.example.flutter_learn
+
+import android.content.Context
+
+object FlutterFragmentUtil {
+    private const val  HIDE_BOTTOM ="HideBottomBar"
+    private const val  TAG ="Flutter"
+
+    fun createFlutterFragment(context:Context,name: String,initRoute: String ="/") :FlutterFragment{
+
+
+    }
+
+}
