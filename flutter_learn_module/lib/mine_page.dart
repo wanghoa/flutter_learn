4e35de7075b11800946528e97e8f9ba31af264db
wanghao <w_hao1987@163.com>
2023-04-15 15:26:47 +0800

    创建 MinePage页面

---
+++ flutter_learn_module/lib/mine_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+
+class MinePage extends StatefulWidget{
+  @override
+  State<MinePage> createState() => _MinePageState();
+
+}
+
+class _MinePageState extends State<MinePage>{
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
