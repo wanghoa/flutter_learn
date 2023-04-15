47a33e228e5bf13933d1284ff8dce4bbc6875879
wanghao <w_hao1987@163.com>
2023-04-16 01:19:29 +0800

    创建我的（MinePage）Fluttter页面

--- flutter_learn_module/lib/mine_page.dart
+++ flutter_learn_module/lib/mine_page.dart
@@ -1,21 +1,24 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_learn_module/widget/Timage.dart';
 
-class MinePage extends StatefulWidget{
+import 'gen/assets.gen.dart';
+
+class MinePage extends StatefulWidget {
   @override
   State<MinePage> createState() => _MinePageState();
-
 }
 
-class _MinePageState extends State<MinePage>{
+class _MinePageState extends State<MinePage> {
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Stack(
+      children: [TImage(Assets.image.defaultPhoto.path)],
+    );
   }
 }
-
