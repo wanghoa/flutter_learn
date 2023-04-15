88b2b40b7623932419f1beaf3e4ee73a3680ca35
wanghao <w_hao1987@163.com>
2023-04-15 16:38:17 +0800

    封装获取图片资源来源

--- flutter_learn_module/lib/widget/Timage.dart
+++ flutter_learn_module/lib/widget/Timage.dart
@@ -1,11 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+
 ///内部判断 image来源（做了一层封装）：是来自asset还是file或memory，network
-class TImage extends StatelessWidget{
+class TImage extends StatelessWidget {
+  double? width;
+  double? height;
+  final String url;
+
+  // url 必传，其他参数可选 使用{}
+  TImage(this.url, {this.width, this.height});
+
   @override
   Widget build(BuildContext context) {
-     if (Image.) {
-
-     }
+    if (url.contains('asset')) {
+      return Image.asset(url, width: width, height: height);
+    } else {
+      return Image.file(File(url), width: width, height: height);
+    }
   }
-
 }
