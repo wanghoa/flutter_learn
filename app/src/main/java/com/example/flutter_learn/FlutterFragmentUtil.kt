cd79f2483098fdc95b0d698e4604eb65a706799e
wanghao <w_hao1987@163.com>
2023-04-15 19:36:41 +0800

    创建FlutterFragmentUtil工具类用户创建FLutterFragment

--- app/src/main/java/com/example/flutter_learn/FlutterFragmentUtil.kt
+++ app/src/main/java/com/example/flutter_learn/FlutterFragmentUtil.kt
@@ -1,12 +1,13 @@
 package com.example.flutter_learn
 
 import android.content.Context
+import io.flutter.embedding.android.FlutterFragment
 
 object FlutterFragmentUtil {
     private const val  HIDE_BOTTOM ="HideBottomBar"
     private const val  TAG ="Flutter"
 
-    fun createFlutterFragment(context:Context,name: String,initRoute: String ="/") :FlutterFragment{
+    fun createFlutterFragment(context:Context,name: String,initRoute: String ="/") : FlutterFragment {
 
 
     }
