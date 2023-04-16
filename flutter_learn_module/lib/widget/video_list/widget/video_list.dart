c90341f0e998360b8dd4abab3096e5f1854ea3a1
wanghao <w_hao1987@163.com>
2023-04-16 21:25:04 +0800

    VideoController 由调用处创建传递

--- flutter_learn_module/lib/widget/video_list/widget/video_list.dart
+++ flutter_learn_module/lib/widget/video_list/widget/video_list.dart
@@ -9,21 +9,23 @@ import '../controller/video_controller.dart';
 
 
 class VideoList extends StatefulWidget {
+  final VideoController controller;
+  VideoList(this.controller);
   @override
   State<StatefulWidget> createState() => _VideoListState();
 }
 
 class _VideoListState extends State<VideoList> {
-  late VideoController _controller;
+
 
   // VideoController _controller = VideoController();
 
   @override
   void initState() {
     super.initState();
-    _controller =
-        VideoController(); //这种初始化方式，如果在_VideoListState被复用场景 一个_VideoListState对应一个 VideoController
-    _controller.init().then((value) => setState(() {}));
+    // controller =
+    //     VideoController(); //这种初始化方式，如果在_VideoListState被复用场景 一个_VideoListState对应一个 VideoController
+    widget.controller.init().then((value) => setState(() {}));
   }
 
   @override
@@ -32,26 +34,26 @@ class _VideoListState extends State<VideoList> {
         body: GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3),
-            itemCount: _controller.dataList?.length,
+            itemCount: widget.controller.dataList?.length,
             itemBuilder: (context, index) {
               //实际项目中，通过DataList[index]获取url
               // var url =
               //     'https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv';
               return GestureDetector(
-                  child: _controller.dataList == null
+                  child: widget.controller.dataList == null
                       ? Container()
                       : AbsorbPointer(
                           absorbing: true,
                           child: VideoView(Player()
                             ..setCommonDataSource(
-                              _controller.dataList![index].url ,
+                              widget.controller.dataList![index].url ,
                               type: SourceType.asset,
                               autoPlay: true,
                             )),
                         ),
                   onTap: () async => await router.push(
                       name: MCRouter.playerPage,
-                      argument: _controller.dataList![index].url));
+                      arguments: widget.controller.dataList![index].url));
             }));
   }
 }
