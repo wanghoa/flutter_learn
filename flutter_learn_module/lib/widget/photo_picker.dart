4ca596f06c8411ccb9324aeb0c14cc92c9d37b46
wanghao <w_hao1987@163.com>
2023-04-16 18:53:38 +0800

    创建PhotoPickerPage

---
+++ flutter_learn_module/lib/widget/photo_picker.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+///展示完整图片；提供切换图片功能
+class PhotoPickerPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
