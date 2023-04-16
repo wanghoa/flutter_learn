8465a78013e2b6fff0f5ae5a9fad3c1c859c2c4c
wanghao <w_hao1987@163.com>
2023-04-16 12:43:06 +0800

    自定义LinearLayoutManager 重写onAttachedToWindow;创建 PagerSnapHelper对象

---
+++ app/src/main/java/com/example/flutter_learn/page/VideoLayoutManager.kt
@@ -0,0 +1,29 @@
+package com.example.flutter_learn.page
+
+import android.content.Context
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.PagerSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * 管理视频列表滑动
+ * 视频布局管理器
+ */
+class VideoLayoutManager(context: Context): LinearLayoutManager(context) {
+    companion object{
+        const val TAG = "Manager"
+
+    }
+    private var mPagerSnapHelper = PagerSnapHelper()
+    private var mRecyclerView: RecyclerView?= null
+
+    override fun onAttachedToWindow(view: RecyclerView?) {
+        super.onAttachedToWindow(view)
+        mPagerSnapHelper.attachToRecyclerView(view)
+        mRecyclerView = view
+    }
+
+    override fun onDetachedFromWindow(view: RecyclerView?, recycler: RecyclerView.Recycler?) {
+        super.onDetachedFromWindow(view, recycler)
+    }
+}
