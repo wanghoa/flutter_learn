e170c8ea35d608e1ba450bc31f56809cc235c9be
wanghao <w_hao1987@163.com>
2023-04-16 22:03:00 +0800

    创建CameraPage

---
+++ flutter_learn_module/lib/camera_page/camera_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CameraPage extends StatefulWidget{
+  @override
+  State<CameraPage> createState() => _CameraPageState ();
+
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
