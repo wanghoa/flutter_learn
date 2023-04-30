59884d90ded948f3e00b1d455c22f4057b9b16fb
wanghao <w_hao1987@163.com>
2023-04-30 19:00:55 +0800

    设置为私有变量

--- flutter_learn_module/lib/mine_page/mine_page.dart
+++ flutter_learn_module/lib/mine_page/mine_page.dart
@@ -5,18 +5,17 @@ import 'package:flutter_learn_module/mine_page/mine_page_controller.dart';
 import 'package:flutter_learn_module/widget/t_image.dart';
 import 'package:get/get.dart';
 
-
 class MinePage extends StatefulWidget {
   @override
   State<MinePage> createState() => _MinePageState();
 }
 
-
-
 class _MinePageState extends State<MinePage> {
   static const image_height = 138.5;
+
   // final _controller = MinePageController();// 这么创建也可以，如果其他页面需要使用controller 需要将它传递过去，所以使用 getX
   final _controller = Get.put(MinePageController());
+
   // @override
   // void initState() {
   //   // TODO: implement initState
@@ -31,16 +30,18 @@ class _MinePageState extends State<MinePage> {
             width: double.infinity,
             height: image_height,
             child: GestureDetector(
-                child: TImage(_controller.backgroundUrl.value, fit: BoxFit.cover),
+                child:
+                    TImage(_controller.backgroundUrl.value, fit: BoxFit.cover),
                 onTap: () async {
                   var fileUrl = await router.push(
                       name: MCRouter.photoPicker,
-                      arguments: {MCRouter.key_url: _controller.backgroundUrl.value});
+                      arguments: {
+                        MCRouter.key_url: _controller.backgroundUrl.value
+                      });
                   if (fileUrl is String) {
                     _controller.backgroundUrl.value = fileUrl;
                     // setState(() {});使用 GetX 不需要setState
                   }
-
                 }))
       ],
     );
