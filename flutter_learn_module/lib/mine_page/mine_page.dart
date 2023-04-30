20509a3e47f0c1c92bcf5af47262518eb33f078e
wanghao <w_hao1987@163.com>
2023-04-30 18:48:40 +0800

    配置'我的'页面管理器

--- flutter_learn_module/lib/mine_page/mine_page.dart
+++ flutter_learn_module/lib/mine_page/mine_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_learn_module/main.dart';
 import 'package:flutter_learn_module/mc_router.dart';
+import 'package:flutter_learn_module/mine_page/mine_page_controller.dart';
 import 'package:flutter_learn_module/widget/t_image.dart';
+import 'package:get/get.dart';
 
-import '../gen/assets.gen.dart';
 
 class MinePage extends StatefulWidget {
   @override
   State<MinePage> createState() => _MinePageState();
 }
 
-String backgroundUrl = Assets.image.defaultPhoto.path;
+
 
 class _MinePageState extends State<MinePage> {
   static const image_height = 138.5;
-
+  // final _controller = MinePageController();// 这么创建也可以，如果其他页面需要使用controller 需要将它传递过去，所以使用 getX
+  final _controller = Get.put(MinePageController());
   // @override
   // void initState() {
   //   // TODO: implement initState
@@ -29,14 +31,14 @@ class _MinePageState extends State<MinePage> {
             width: double.infinity,
             height: image_height,
             child: GestureDetector(
-                child: TImage(backgroundUrl, fit: BoxFit.cover),
+                child: TImage(_controller.backgroundUrl.value, fit: BoxFit.cover),
                 onTap: () async {
                   var fileUrl = await router.push(
                       name: MCRouter.photoPicker,
-                      arguments: {MCRouter.key_url: backgroundUrl});
+                      arguments: {MCRouter.key_url: _controller.backgroundUrl.value});
                   if (fileUrl is String) {
-                    backgroundUrl = fileUrl;
-                    setState(() {});
+                    _controller.backgroundUrl.value = fileUrl;
+                    // setState(() {});使用 GetX 不需要setState
                   }
 
                 }))
--- flutter_learn_module/lib/mine_page/mine_page_controller.dart
+++ flutter_learn_module/lib/mine_page/mine_page_controller.dart
@@ -1,6 +1,9 @@
 import 'package:get/get.dart';
 
+import '../gen/assets.gen.dart';
+// GetX 改造步骤2，继承GetController
 class MinePageController extends GetxController {
   //GetX 改造步骤3 给变量添加 .obs
+  var  backgroundUrl = Assets.image.defaultPhoto.path.obs;
 
 }
