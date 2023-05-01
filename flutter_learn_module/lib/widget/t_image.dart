67142b00bd985e91f2d75b00af8620dd161e205f
wanghao <w_hao1987@163.com>
2023-05-01 13:46:02 +0800

    拓展 展示圆角图片

--- flutter_learn_module/lib/widget/t_image.dart
+++ flutter_learn_module/lib/widget/t_image.dart
@@ -7,17 +7,39 @@ class TImage extends StatelessWidget {
   double? width;
   double? height;
   final String url;
-  final fit;// 裁剪 做屏幕适配
+  final fit; // 裁剪 做屏幕适配
+  final shape;
+  double? radius;
 
   // url 必传，其他参数可选 使用{}
-  TImage(this.url, {this.width, this.height, this.fit = BoxFit.cover});
+  TImage(this.url,
+      {this.width,
+      this.height,
+      this.fit = BoxFit.cover,
+      this.shape = Shape.NORMAL,
+      this.radius});
 
   @override
   Widget build(BuildContext context) {
-    if (url.contains('asset')) {
-      return Image.asset(url, width: width, height: height, fit: fit);
-    } else {
-      return Image.file(File(url), width: width, height: height, fit: fit);
+    switch (shape) {
+      case Shape.NORMAL:
+        if (url.contains('asset')) {
+          return Image.asset(url, width: width, height: height, fit: fit);
+        } else {
+          return Image.file(File(url), width: width, height: height, fit: fit);
+        }
+      case Shape.CIRCLE:
+        return Container(
+            width: radius != null ? radius! * 2 + 8 : null,
+            height: radius != null ? radius! * 2 + 8 : null);
+      default:
+        if (url.contains('asset')) {
+          return Image.asset(url, width: width, height: height, fit: fit);
+        } else {
+          return Image.file(File(url), width: width, height: height, fit: fit);
+        }
     }
   }
 }
+
+enum Shape { NORMAL, CIRCLE }
