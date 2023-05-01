4a4f22b826735fdc15fa14bc286b62aef16c85a1
wanghao <w_hao1987@163.com>
2023-05-01 13:31:24 +0800

    创建 view 展示的数据

--- flutter_learn_module/lib/mine_page/mine_page_controller.dart
+++ flutter_learn_module/lib/mine_page/mine_page_controller.dart
@@ -10,4 +10,28 @@ class MinePageController extends GetxController {
   String get backgroundUrl => _backgroundUrl.value;
 
   set backgroundUrl(String url) => _backgroundUrl.value = url;
+
+  String get likeCount => '23万';
+
+  String get focusCount => '85225';
+
+  String get followCount => '12567';
+
+  var _avatarUrl = Assets.image.avatar.path.obs;
+
+  var _name = 'wanghao'.obs;
+  var _uid = '365'.obs;
+
+  String get avatarUrl => _avatarUrl.value;
+
+  set avatarUrl(String url) => _avatarUrl.value = url;
+
+  String get name => _name.value;
+
+  set name(String name) => _name.value = name;
+
+  // String get uidDesc => _uid.value;
+  String get uidDesc => '幕客:${_uid.value}';
+
+  set uidDesc(String desc) => _uid.value = desc;
 }
