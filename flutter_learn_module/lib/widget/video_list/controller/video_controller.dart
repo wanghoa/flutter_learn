228e2cbdd67140f7165c1481039672dcd3c34ad4
wanghao <w_hao1987@163.com>
2023-05-01 17:18:42 +0800

    定义抽象VideoController

--- flutter_learn_module/lib/widget/video_list/controller/video_controller.dart
+++ flutter_learn_module/lib/widget/video_list/controller/video_controller.dart
@@ -1,69 +1,44 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../model/video_model.dart';
 import '../server_data.dart';
 
+abstract class VideoController {
+  ///针对不同类型视频，采用不同的key存储
+  String get spKey;
 
-class VideoController {
-  // static const data1 = {
-  // "title": "实例视频"，
-  // "url": "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
-  // "playCount": 66
-  // }
+  /// 返回不同的视频内容
+  String get videoData;
 
-  // static const _serverData = """{
-  // "title":"实例视频"，
-  // "url","https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
-  // "playCount":66}  """; //三引号内的为字符串数据  模拟服务端返回的json数据
-
-  // Map<String, dynamic> _videoData = {};//第一种方案
-  List<VideoModel> ? dataList; //第二种方案
+  List<VideoModel>? dataList; //第二种方案
+  int get count => Random().nextInt(1000);
 
   Future<void> init() async {
-    // _videoData = fetchVideoData();//第一种方案
-    // model = fetchVideoData2();//第二种方案
-    dataList ??= (await fetchVideoData3()) ;
-    // if (model == null) {      // 首先判断一级缓存 既内存中是否有数据
-    //   // 没有，从二级或三级缓存中查找 then 为异步执行
-    //   // fetchVideoData3().then((value)=>model = value);//先执行方法 获取到value值 ，再将value赋值给model
-    //   model = await fetchVideoData3(); // 使用await 会阻塞流程；就是init方法执行后，程序才会向下执行其他方法如 ：VideoList的生命周期build()
-    // }
+    dataList ??= (await fetchVideoData3());
   }
-
-// 从Map中获取数据 //第一种方案
-//   String get title => _videoData['title'];
-//   String get url => _videoData['url'];
-//   int get playCount => _videoData['playCount'];
 
   /**
    * 从服务端获取json字符串 类型表示的视频数据
    */
-  // Map<String, dynamic> fetchVideoData() {//第一种方案
-  //   return jsonDecode(_serverData); // json转 map
-  // }
 
-  // VideoModel fetchVideoData2() {//第二种方案
-  //   return VideoModel.fromJson(jsonDecode(_serverData));
-  // }
   // 增加缓存逻辑  第三种优化方案
-  Future<List<VideoModel>> fetchVideoData3() async{
-    var sp  = await SharedPreferences.getInstance();
-    var modelStr = sp.getString('videoModel');
+  Future<List<VideoModel>> fetchVideoData3() async {
+    var sp = await SharedPreferences.getInstance();
+    var modelStr = sp.getString(spKey);
     if (modelStr != null && modelStr.isNotEmpty) {
       // 二级缓存中有数据直接使用 。（一级缓存是内存）
       var list = jsonDecode(modelStr) as List<dynamic>;
       return list.map((e) => VideoModel.fromJson(e)).toList();
-    }else{
+    } else {
       //三级缓存
-      var list = jsonDecode(ServerData.fetchDataFromServer());
+      var list = jsonDecode(videoData);
       var sp = await SharedPreferences.getInstance();
-      sp.setString('videoModel', ServerData.fetchDataFromServer());
+      sp.setString(spKey, videoData);
 
       return list.map((e) => VideoModel.fromJson(e)).toList();
-      // return VideoModel.fromJson(model);// List<dynamic> ia not a subtype fo type
     }
-    // return VideoModel.fromJson(jsonDecode(ServerData.fetchDataFromServer()));
   }
 }
