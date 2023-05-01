9077c8f73caf74fb48069fb3fb9d5756d98ffcd9
wanghao <w_hao1987@163.com>
2023-05-02 02:03:10 +0800

    完善相机页面逻辑

--- flutter_learn_module/lib/camera_page/camera_page.dart
+++ flutter_learn_module/lib/camera_page/camera_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_learn_module/camera_page/camera_page_controller.dart';
 import 'package:flutter_learn_module/gen/assets.gen.dart';
 import 'package:flutter_learn_module/widget/t_image.dart';
+import 'package:get/get.dart';
 
 class CameraPage extends StatefulWidget {
   @override
@@ -12,21 +13,30 @@ class CameraPage extends StatefulWidget {
 
 class _CameraPageState extends State<CameraPage> {
   late CameraPageController _cameraController;
+  late CameraController _cameraControllerNative;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _cameraController = CameraPageController();
+    _cameraController.init();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return MaterialApp(
+        home: Stack(
       children: [
         Container(
-          color: Colors.blue,
-        ),
+            width: double.infinity,
+            height: double.infinity,
+            child: Obx(() {
+              return _cameraController.cameraController == null ||
+                      !_cameraController.cameraController!.value.isInitialized
+                  ? Container(color: Colors.black)
+                  : CameraPreview(_cameraController.cameraController!);
+            })),
         Padding(
             padding: EdgeInsets.only(top: 38, left: 19),
             child: GestureDetector(
@@ -34,8 +44,50 @@ class _CameraPageState extends State<CameraPage> {
               onTap: () {
                 _cameraController.onTapClose();
               },
-            ))
+            )),
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: EdgeInsets.only(top: 35, right: 14),
+            child: Column(
+              children: [
+                _buildIcon(Assets.image.close.path, '翻转',
+                    () => _cameraController.onSwitchCamera()),
+                SizedBox(height: 16),
+                _buildIcon(
+                    Assets.image.close.path,
+                    '倒计时',
+                    () => Future.delayed(Duration(seconds: 3),
+                        () => _cameraController.takePhotoAndUpload())),
+                SizedBox(height: 16),
+                Obx(() => _buildIcon(
+                    _cameraController.flash
+                        ? Assets.image.close.path
+                        : Assets.image.close.path,
+                    '闪光灯',
+                    () => _cameraController.onSwitchCamera()))
+              ],
+            ),
+          ),
+        )
       ],
-    );
+    ));
+  }
+
+  Widget _buildIcon(String iconPath, String title, GestureTapCallback onTap) {
+    return GestureDetector(
+        child: Column(children: [
+          TImage(
+            iconPath,
+            width: 25,
+            height: 25,
+          ),
+          Text(title,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 10,
+                  decoration: TextDecoration.none))
+        ]),
+        onTap: onTap);
   }
 }
--- flutter_learn_module/lib/camera_page/camera_page_controller.dart
+++ flutter_learn_module/lib/camera_page/camera_page_controller.dart
@@ -1,9 +1,77 @@
+import 'package:camera/camera.dart';
 import 'package:flutter_learn_module/channel_util.dart';
 import 'package:get/get.dart';
 
 class CameraPageController extends GetxController {
+  var _cameraIndex = 0.obs;
+  var _recording = false.obs;
+  var _flash = false.obs;
+
+  CameraController? _cameraController;
+  late List<CameraDescription> _cameras;
+  var _cameraControllerObs = Rx<CameraController?>(null);
+
   void onTapClose() {
 // 调用MethodChannel 通知Native 关闭flutter 容器
     ChannelUtil.closeCamera();
+  }
+
+  void init() {
+    //1.等待camera 初始化
+    availableCameras().then((value) {
+      _cameras = value;
+      _initCameraController();
+    });
+  }
+
+  CameraController? get cameraController => _cameraControllerObs.value;
+
+  int get cameraIndex => _cameraIndex.value;
+
+  bool get flash => _flash.value;
+
+  set flash(bool enable) => _flash.value = enable;
+
+  set cameraIndex(int cameraIndex) => _cameraIndex.value = cameraIndex;
+
+  bool get recording => _recording.value;
+
+  set recording(bool recording) => _recording.value = recording;
+
+  void _initCameraController() {
+    //2.选择具体camera 来创建cameraController
+    _cameraController =
+        CameraController(_cameras[cameraIndex], ResolutionPreset.max);
+    //3.等待cameraController初始化
+    _cameraController?.initialize().then((_) {
+      _cameraControllerObs.value = _cameraController;
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  }
+
+  void onSwitchCamera() {
+    if (_cameras.length > 1) {
+      cameraIndex = cameraIndex == 0 ? 1 : 0;
+      _initCameraController();
+    }
+  }
+
+  Future<void> takePhotoAndUpload() async {
+    if (recording) {
+      recording = false;
+      XFile? file = await _cameraController?.stopVideoRecording();
+      print('video_path: ${file!.path}');
+    } else {
+      recording = true;
+      _cameraController?.startVideoRecording();
+    }
   }
 }
