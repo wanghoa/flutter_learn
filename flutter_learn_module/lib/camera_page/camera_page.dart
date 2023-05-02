e229eb27369728bcd19f79d53ec4549895499541
wanghao <w_hao1987@163.com>
2023-05-02 15:18:41 +0800

    打开相册

--- flutter_learn_module/lib/camera_page/camera_page.dart
+++ flutter_learn_module/lib/camera_page/camera_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter_learn_module/camera_page/camera_page_controller.dart';
 import 'package:flutter_learn_module/gen/assets.gen.dart';
 import 'package:flutter_learn_module/widget/t_image.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraPage extends StatefulWidget {
   @override
@@ -27,51 +28,76 @@ class _CameraPageState extends State<CameraPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Stack(
-      children: [
-        Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Obx(() {
-              return _cameraController.cameraController == null ||
+          children: [
+            Container(
+                width: double.infinity,
+                height: double.infinity,
+                child: Obx(() {
+                  return _cameraController.cameraController == null ||
                       !_cameraController.cameraController!.value.isInitialized
-                  ? Container(color: Colors.black)
-                  : CameraPreview(_cameraController.cameraController!);
-            })),
-        Padding(
-            padding: EdgeInsets.only(top: 38, left: 19),
-            child: GestureDetector(
-              child: TImage(Assets.image.close.path, height: 18, width: 18),
-              onTap: () {
-                _cameraController.onTapClose();
-              },
-            )),
-        Align(
-          alignment: Alignment.topRight,
-          child: Padding(
-            padding: EdgeInsets.only(top: 35, right: 14),
-            child: Column(
-              children: [
-                _buildIcon(Assets.image.close.path, '翻转',
-                    () => _cameraController.onSwitchCamera()),
-                SizedBox(height: 16),
-                _buildIcon(
-                    Assets.image.close.path,
-                    '倒计时',
-                    () => Future.delayed(Duration(seconds: 3),
-                        () => _cameraController.takePhotoAndUpload())),
-                SizedBox(height: 16),
-                Obx(() => _buildIcon(
-                    _cameraController.flash
-                        ? Assets.image.close.path
-                        : Assets.image.close.path,
-                    '闪光灯',
-                    () => _cameraController.onSwitchCamera()))
-              ],
+                      ? Container(color: Colors.black)
+                      : CameraPreview(_cameraController.cameraController!);
+                })),
+            Padding(
+                padding: EdgeInsets.only(top: 38, left: 19),
+                child: GestureDetector(
+                  child: TImage(Assets.image.close.path, height: 18, width: 18),
+                  onTap: () {
+                    _cameraController.onTapClose();
+                  },
+                )),
+            Align(
+              alignment: Alignment.topRight,
+              child: Padding(
+                padding: EdgeInsets.only(top: 35, right: 14),
+                child: Column(
+                  children: [
+                    _buildIcon(Assets.image.rotate.path, '翻转',
+                            () => _cameraController.onSwitchCamera()),
+                    SizedBox(height: 16),
+                    _buildIcon(
+                        Assets.image.clock.path,
+                        '倒计时',
+                            () =>
+                            Future.delayed(Duration(seconds: 3),
+                                    () =>
+                                    _cameraController.takePhotoAndUpload())),
+                    SizedBox(height: 16),
+                    Obx(() =>
+                        _buildIcon(
+                            _cameraController.flash
+                                ? Assets.image.flashOff.path
+                                : Assets.image.flashOn.path,
+                            '闪光灯',
+                                () => _cameraController.onSwitchCamera()))
+                  ],
+                ),
+              ),
             ),
-          ),
-        )
-      ],
-    ));
+
+            Align(alignment: Alignment.bottomLeft,
+              child: Padding(padding: EdgeInsets.only(left: 62, bottom: 110),
+                  child: GestureDetector(child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TImage(Assets.image.gallery.path, height: 40, width: 40),
+                      SizedBox(height: 10),
+                      Text('相册', style: TextStyle(color: Colors.white,
+                          fontSize: 12,
+                          decoration: TextDecoration.none),)
+                    ],), onTap: () async {
+                    var pickedFile = await ImagePicker().pickVideo(
+                        source: ImageSource.gallery);
+                    var path = pickedFile ?.path;
+                    if (path != null) {
+
+
+                    }
+                  },)
+              ),),
+          ],
+        ));
   }
 
   Widget _buildIcon(String iconPath, String title, GestureTapCallback onTap) {
